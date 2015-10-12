d6717763024412040df7606915f318474465102b
Michael Bayne <mdb@samskivert.com>
2015-10-11 21:33:56 -0700

    Handle multiple packages.

--- bin/build-test.sh
+++ bin/build-test.sh
@@ -4,12 +4,10 @@
 
 set -e
 
-PKGURL=$1
-if [ -z "$PKGURL" ]; then
-  echo "Usage: $0 package_url"
+if [ -z "$1" ]; then
+  echo "Usage: $0 package_url [package_url ...]"
   exit 255
 fi
-PACKAGE=`basename $PKGURL .git`
 
 # create and clean our temp build directory
 mkdir -p target/spam
@@ -24,16 +22,22 @@ RUNSPAM="java -jar $SPAM/scaled-pacman.jar -d"
 
 export SCALED_HOME=$SPAM
 
-# install/build the package
-$RUNSPAM install $PKGURL
+while [ ! -z "$1" ]; do
+  # install/build the package
+  PKGURL=$1
+  PACKAGE=`basename $PKGURL .git`
+  $RUNSPAM install $PKGURL
 
-# then run our tests if we have any
-TESTDIR=$SPAM/Packages/$PACKAGE/test
-if [ -d $TESTDIR ]; then
-  cd $TESTDIR
-  $RUNSPAM run $PACKAGE#test org.junit.runner.JUnitCore \
-    `find target/classes -name '*Test.class' | \
-     sed 's:target/classes/::' | sed 's:.class::' | sed 's:/:.:g'`
-else
-  echo "No $TESTDIR. Not running tests."
-fi
+  # then run our tests if we have any
+  TESTDIR=$SPAM/Packages/$PACKAGE/test
+  if [ -d $TESTDIR ]; then
+    cd $TESTDIR
+    $RUNSPAM run $PACKAGE#test org.junit.runner.JUnitCore \
+      `find target/classes -name '*Test.class' | \
+       sed 's:target/classes/::' | sed 's:.class::' | sed 's:/:.:g'`
+  else
+    echo "No $TESTDIR. Not running tests."
+  fi
+
+  shift
+done
